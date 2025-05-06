94aafc16eb49194f83be262cdc0b58c54b74621e
x5113nc3x <vadimpirogovskiy2003@gmail.com>
2025-05-07 00:16:22 +0700

    change to work version

--- test_prog.c
+++ test_prog.c
@@ -1,18 +1,18 @@
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
+#include <string.h>
+#include <sys/prctl.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/prctl.h>
-#include <errno.h>
-#include <string.h>
-
+#include <time.h>
+#include <unistd.h>
 
 volatile sig_atomic_t running = 1;
 
-int check_debugger() {
+int enable_traceme() {
     if (ptrace(PTRACE_TRACEME, 0, 0, 0) == -1) {
         if (errno == EPERM) {
             return 1;
@@ -21,34 +21,17 @@ int check_debugger() {
         ptrace(PTRACE_DETACH, 0, 0, 0);
     }
 
-    // FILE *f = fopen("/proc/self/status", "r");
-    // if (f) {
-    //     char line[256];
-    //     while (fgets(line, sizeof(line), f)) {
-    //         if (strncmp(line, "TracerPid:", 10) == 0) {
-    //             printf("TracerPid: %s", line + 10);
-    //             int tracer_pid = atoi(line + 10);
-    //             if (tracer_pid != 0) {
-    //                 fclose(f);
-    //                 return 1;
-    //             }
-    //             break;
-    //         }
-    //     }
-    //     fclose(f);
-    // }
-    
     return 0;
 }
 
-void anti_debug_check() {
+void disable_debugger() {
     static time_t last_check = 0;
     time_t now = time(NULL);
-    
+
     if (now - last_check >= 2) {
         last_check = now;
-        
-        if (check_debugger()) {
+
+        if (enable_traceme()) {
             printf("\n!!! Обнаружена попытка отладки! Завершение работы !!!\n");
             fflush(stdout);
             exit(1);
@@ -64,31 +47,25 @@ void handle_signal(int sig) {
 int main() {
     printf("PID программы: %d\n", getpid());
 
-    prctl(PR_SET_DUMPABLE, 0);
-
-    #ifdef PR_SET_PTRACER
-    prctl(PR_SET_PTRACER, 0);
-    #endif
-    
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
-    
+    disable_debugger();
+
     int counter = 1;
-    
+
     while (running) {
-        anti_debug_check();
         printf("Текущее число: %d из 20\n", counter);
         fflush(stdout);
-        
+
         sleep(1);
-        
+
         counter++;
         if (counter > 20) {
             counter = 1;
             printf("--------- Начинаем новый цикл ---------\n");
         }
     }
-    
+
     printf("Программа завершена\n");
     return 0;
 }
